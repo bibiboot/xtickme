0e48b0cf86ae12b808db75020b6c186a23c4d80a
Danish Rehman <rehman.d.fazlur@gmail.com>
2014-01-25 13:39:08 -0800

    Adding bend

--- public/js/engine.js
+++ public/js/engine.js
@@ -3,6 +3,10 @@
         if(eventid == 'fuck'){
             fuck_engine(user);
         }
+        else if(eventid == 'bend'){
+            bend_engine(user);
+
+        }
     }
 
     function fuck_engine(user){
@@ -11,5 +15,14 @@
         }
         else {
          requestAnimationFrameID = window.requestAnimationFrame(fuck2);
+        }
+    }
+
+    function bend_engine(user){
+        if(user == 1) {
+           requestAnimationFrameID = window.requestAnimationFrame(bend);
+        }
+        else {
+         requestAnimationFrameID = window.requestAnimationFrame(bend2);
         }
     }
